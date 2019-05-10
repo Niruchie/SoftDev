bb9dd11a2b744806030cafe60fbb0a370dd6a352
Hakerio <hakerio08@hotmail.com>
2019-05-09 23:30:15 -0600

    Terminado...

--- Extraclase.c
+++ Extraclase.c
@@ -1,17 +1,17 @@
 /*************************************************************************************************
-Aqui estaria la licencia, si tuviera una...
-No me gustan los comentarios;
-Pero, los hago para que vean que si los sé hacer...
-Y para que se entienda de mejor forma lo que estoy haciendo...
-Pero para eso existen los nombres de varibles bien hechos :P...
-Este trabajo esta muy ordenado...
-
-Hexadecimales para las tildes y eñes:
-	0xA0 == 160: a tildada ;
-	0xA1 == 161: i tildada ;
-	0xA2 == 162: o tildada ;
-	0xA3 == 163: u tildada ;
-	0xA4 == 164: Letra ñ   ;
+#                           Aqui estaria la licencia, si tuviera una...                          #
+#                                  No me gustan los comentarios;                                 #
+#                       Pero, los hago para que vean que si los sé hacer...                      #
+#                  Y para que se entienda de mejor forma lo que estoy haciendo...                #
+#                  Pero para eso existen los nombres de varibles bien hechos :P...               #
+#                                Este trabajo esta muy ordenado...                               #
+#																								 #
+#								Hexadecimales para las tildes y eñes:                            #
+#									0xA0 == 160: a tildada ;									 #
+#									0xA1 == 161: i tildada ;									 #
+#									0xA2 == 162: o tildada ;									 #
+#									0xA3 == 163: u tildada ;									 #
+#									0xA4 == 164: Letra ñ   ;									 #
 *************************************************************************************************/
 
 //Incluimos librerias;
@@ -52,14 +52,14 @@ main(){
 		strcpy(OSM[2], "3) Salir");
 	
 	//SUB: Subjects (Asignaturas en ingles);
-	char SUB[7][80];
-		strcpy(SUB[0], "1) Espa%col\n");
-		strcpy(SUB[1], "2) Estudios Sociales\n");
-		strcpy(SUB[2], "3) Matem%cticas\n");
-		strcpy(SUB[3], "4) Ciencias (F%csica)\n");
-		strcpy(SUB[4], "5) Ingles Acad%cmico\n");
-		strcpy(SUB[5], "6) Ingles T%ccnico\n");
-		strcpy(SUB[6], "7) Salir\n");
+	char SUB[8][80];
+		strcpy(SUB[1], "1) Espa%col\n");
+		strcpy(SUB[2], "2) Estudios Sociales\n");
+		strcpy(SUB[3], "3) Matem%cticas\n");
+		strcpy(SUB[4], "4) Ciencias (F%csica)\n");
+		strcpy(SUB[5], "5) Ingles Acad%cmico\n");
+		strcpy(SUB[6], "6) Ingles T%ccnico\n");
+		strcpy(SUB[7], "7) Salir\n");
 	
 	char TALLER[2][20];
 		strcpy(TALLER[0], "1) Deficiencias\n");
@@ -90,21 +90,27 @@ main(){
 	
 	char DEFICIENCIAS[10][75];
 		strcpy(DEFICIENCIAS[0], "Bueno, entre las deficiencias la verdad nunca las habia pensado... \n");
-		strcpy(DEFICIENCIAS[1], "[1] La forma de comunicar las cosas a veces se me hace dificil   \n");
-		strcpy(DEFICIENCIAS[2], "[+] no encuentro muy bien los modos de presentar enfrente de       \n");
-		strcpy(DEFICIENCIAS[3], "[+] algunas personas, pero muchos me dicen que lo hago bien, asi   \n");
-		strcpy(DEFICIENCIAS[4], "[+] que pienso que es confianza lo que debo de agarrar.            \n");
+		strcpy(DEFICIENCIAS[1], "\t[1] La forma de comunicar las cosas a veces se me hace dificil     \n");
+		strcpy(DEFICIENCIAS[2], "\t[+] no encuentro muy bien los modos de presentar enfrente de       \n");
+		strcpy(DEFICIENCIAS[3], "\t[+] algunas personas, pero muchos me dicen que lo hago bien, asi   \n");
+		strcpy(DEFICIENCIAS[4], "\t[+] que pienso que es confianza lo que debo de agarrar.            \n");
 		
-		strcpy(DEFICIENCIAS[5], "[2] Me cuesta aceptar la forma en que otros ordenan sus trabajos \n");
-		strcpy(DEFICIENCIAS[6], "[+] a veces me pongo a ver la forma en la cual los otros trabajan y\n");
-		strcpy(DEFICIENCIAS[7], "[+] no me gusta, considero que podria ser un problema en Expotec y \n");
-		strcpy(DEFICIENCIAS[8], "[+] a lo mejor se hara una molestia, pero siempre trato que lo     \n");
-		strcpy(DEFICIENCIAS[9], "[+] corrijan para que se vea bonito o yo adaptarme y no perderme.  \n");
+		strcpy(DEFICIENCIAS[5], "\t[2] Me cuesta aceptar la forma en que otros ordenan sus trabajos \n");
+		strcpy(DEFICIENCIAS[6], "\t[+] a veces me pongo a ver la forma en la cual los otros trabajan y\n");
+		strcpy(DEFICIENCIAS[7], "\t[+] no me gusta, considero que podria ser un problema en Expotec y \n");
+		strcpy(DEFICIENCIAS[8], "\t[+] a lo mejor se hara una molestia, pero siempre trato que lo     \n");
+		strcpy(DEFICIENCIAS[9], "\t[+] corrijan para que se vea bonito o yo adaptarme y no perderme.  \n");
 	
 	char FORTALEZAS[10][75];
 		strcpy(FORTALEZAS[0], "La verdad es que si creo que la mayor parte pertenecen aqui:          \n");
-		strcpy(FORTALEZAS[0], "\t[1] Entiendo muy bien el proceso logico por el cual pasan las cosas \n");
-		strcpy(FORTALEZAS[0], "\t[+]\n");
+		strcpy(FORTALEZAS[1], "\t[1] Entiendo muy bien el proceso logico por el cual pasan las cosas \n");
+		strcpy(FORTALEZAS[2], "\t[+] y como es que suceden conforme se realizan.\n");
+		strcpy(FORTALEZAS[3], "\t[2] Me gusta investigar hasta hallar la solucion a un problema      \n");
+		strcpy(FORTALEZAS[4], "\t[+] que se presenta cuando hago algo.\n");
+		strcpy(FORTALEZAS[5], "\t[3] Desde los 4 me gustaba el hecho de saber como la tecnologia\n");
+		strcpy(FORTALEZAS[6], "\t[+] funcionaba, ya que me educaron sobre ello y como controlarlo.\n");
+		strcpy(FORTALEZAS[7], "\t[4] Soy autodicdacta, aprendo por mi mismo, casi sin nesecidad de\n");
+		strcpy(FORTALEZAS[8], "\t[+] que me comenten sobre lo que se hace, solo presto atencion.\n");
 	
 	//Comienza con el nombre y la sección que no se repetirán;
 	system("cls");
@@ -127,13 +133,13 @@ main(){
 				do{
 					//Imprime el submenú y pide una opción;
 					system("cls");
-					printf(SUB[0], 0xA4);       //Español,     eñe   ;
-					printf(SUB[1]);
-					printf(SUB[2], 0xA0);       //Matemáticas, tilde ;
-					printf(SUB[3], 0xA1);       //Física,      tilde ;
-					printf(SUB[4], 0x82);       //Académico,   tilde ;
-					printf(SUB[5], 0x82);       //Técnico,     tilde ;
-					printf(SUB[6]);
+					printf(SUB[1], 164);       //Español,     eñe   ;
+					printf(SUB[2]);
+					printf(SUB[3], 0xA0);       //Matemáticas, tilde ;
+					printf(SUB[4], 0xA1);       //Física,      tilde ;
+					printf(SUB[5], 0x82);       //Académico,   tilde ;
+					printf(SUB[6], 0x82);       //Técnico,     tilde ;
+					printf(SUB[7]);
 					printf("Escriba una opci%cn: X\b", 0xA2);
 					scanf("%d", &optSuj);
 					
@@ -349,13 +355,15 @@ main(){
 								system("pause>nul");
 								for(j=0; j<69; j++) printf("\b");
 							}
+							break;
 						case 2:
-							for(i=0; i<10; i++){
+							for(i=0; i<9; i++){
 								printf(FORTALEZAS[i]);
-								if(i<9) printf("----------------------------> MAS <----------------------------");
+								if(i<8) printf("----------------------------> MAS <----------------------------");
 								system("pause>nul");
 								for(j=0; j<69; j++) printf("\b");
 							}
+							break;
 					}
 				}while(optSuj != SALIR);
 				break;
@@ -372,11 +380,3 @@ main(){
 		}
 	}while(opt != SALIR);
 }
-/*
-printf(NOTAS[0], 0xA4, 0xA1); //Español,     eñes  ;
-printf(NOTAS[1]);
-printf(NOTAS[2], 0xA0);       //Matemáticas, tilde ;
-printf(NOTAS[3], 0xA1);       //Física,      tilde ;
-printf(NOTAS[4], 0x82);       //Académico,   tilde ;
-printf(NOTAS[5], 0x82);       //Técnico,     tilde ;
-*/
